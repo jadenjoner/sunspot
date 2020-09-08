492bb0993737792ada3ddedb838115cc3d30ece8
Jaden <jadenjoner@gmail.com>
2020-09-08 11:41:09 -0700

    removed a console log

--- js/prespective.js
+++ js/prespective.js
@@ -10,7 +10,6 @@ setInterval(() => {
   prescroll = (prescroll*9+scroll)/10
   var diff = Math.abs(scroll-prescroll)
   if(diff>500){
-    console.log('diff', scroll - prescroll)
     prescroll = (prescroll*(diff/100-1)+scroll)/(diff/100)
   }
 
